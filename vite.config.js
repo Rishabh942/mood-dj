ebf9e393bfb002bbed29f50ae68aafb5b99f9b47
williamtzhou <williamtzhou@gmail.com>
2025-09-21 03:55:17 -0400

    deaduzz

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ export default defineConfig({
   root: '.',                 // root is the folder with index.html (repo root)
   publicDir: 'client/public',
   plugins: [react()],
-  server: { host: '127.0.0.1', port: 5173 },
   build: {
     outDir: 'dist',
     emptyOutDir: true,
